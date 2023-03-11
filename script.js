929da1fc27678248f28a7fc5b646b0e63c236ab9
Ghullam Mustafa <gmustafawaince180@gmail.com>
2023-03-11 23:28:49 +0500

    two

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ console.log(one+two);
 
 var one = 1100;
 var two = 586;
-// console.log(one+two);
+console.log(one+two);
 // console.log(one*two);
 // console.log(one/two);
 // console.log(one%two);
