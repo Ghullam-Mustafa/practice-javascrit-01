bd3d9254b59c2e6d884551d0fc23357ebe669fe2
Ghullam Mustafa <gmustafawaince180@gmail.com>
2023-03-19 18:05:12 +0500

    m

--- script.js
+++ script.js
@@ -1,76 +1,76 @@
-var one = 3;
-var two = 4;
-console.log(one * two);
+// var one = 3;
+// var two = 4;
+// console.log(one * two);
 
-var num = 2;
-var floatingData = 2.4;
-console.log(num,floatingData)
-var one = 5;
-var two = 5;
-console.log(one+two);
+// var num = 2;
+// var floatingData = 2.4;
+// console.log(num,floatingData)
+// var one = 5;
+// var two = 5;
+// console.log(one+two);
 
-var one = 1100;
-var two = 586;
-console.log(one+two);
-console.log(one*two);
-console.log(one/two);
-console.log(one%two);
+// var one = 1100;
+// var two = 586;
+// console.log(one+two);
+// console.log(one*two);
+// console.log(one/two);
+// console.log(one%two);
 
-var string = `cdcd`
-console.log(string)
+// var string = `cdcd`
+// console.log(string)
 
-var floating = 46845.3484
-console.log(floating)
+// var floating = 46845.3484
+// console.log(floating)
 
-var cancadinaton = "8"
-console.log(cancadinaton+6)
+// var cancadinaton = "8"
+// console.log(cancadinaton+6)
 
-var string = `cdcd`
-console.log(string)
+// var string = `cdcd`
+// console.log(string)
 
-var floating = 46845.3484
-console.log(floating)
+// var floating = 46845.3484
+// console.log(floating)
 
-var cancadinaton = "8"
-console.log(cancadinaton+6)
+// var cancadinaton = "8"
+// console.log(cancadinaton+6)
 
-var string = `cdcd`
-console.log(string)
+// var string = `cdcd`
+// console.log(string)
 
-var floating = 46845.3484
-console.log(floating)
+// var floating = 46845.3484
+// console.log(floating)
 
-var cancadinaton = "8"
-console.log(cancadinaton+6)
+// var cancadinaton = "8"
+// console.log(cancadinaton+6)
 
-var string = `cdcd`
-console.log(string)
+// var string = `cdcd`
+// console.log(string)
 
-var floating = 46845.3484
-console.log(floating)
+// var floating = 46845.3484
+// console.log(floating)
 
-var cancadinaton = "8"
-console.log(cancadinaton+6)
+// var cancadinaton = "8"
+// console.log(cancadinaton+6)
 
-var string = `cdcd`
-console.log(string)
+// var string = `cdcd`
+// console.log(string)
 
-var floating = 46845.3484
-console.log(floating)
+// var floating = 46845.3484
+// console.log(floating)
 
-var cancadinaton = "8"
-console.log(cancadinaton+6)
+// var cancadinaton = "8"
+// console.log(cancadinaton+6)
 
-var add = 4 > 3 ;
-console.log(add)
+// var add = 4 > 3 ;
+// console.log(add)
 
-var add1 = 5 < 3 ;
-console.log(add1)
+// var add1 = 5 < 3 ;
+// console.log(add1)
 
-var add2 = prompt("enter any ")
-console.log(add2 > 5)
+// var add2 = prompt("enter any ")
+// console.log(add2 > 5)
 
-var add3 = prompt("enter any ")
-console.log(add3 < 5)
+// var add3 = prompt("enter any ")
+// console.log(add3 < 5)
 
-document.write("he")
+// document.write("he")
