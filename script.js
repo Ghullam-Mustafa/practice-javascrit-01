899c34856039c7355bd7e340c2055ff336397890
Ghullam Mustafa <gmustafawaince180@gmail.com>
2023-03-20 22:07:55 +0500

    h

--- script.js
+++ script.js
@@ -86,8 +86,8 @@ if (inputUser >=0) {
   alert(inputUser);
 }
 
-var inputvalue1 = prompt("enter value 1")
-var inputvalue2 = prompt("enter value 2")
-if (inputvalue1 == inputvalue2){
-    alert(inputvalue1,inputvalue2);
-}
+// var inputvalue1 = prompt("enter value 1")
+// var inputvalue2 = prompt("enter value 2")
+// if (inputvalue1 == inputvalue2){
+//     alert(inputvalue1,inputvalue2);
+// }
