f9db2d9d7e56ece29391db621b342e4f50e3ee02
Ghullam Mustafa <gmustafawaince180@gmail.com>
2023-04-13 22:25:08 +0500

    check variable

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
-// document.getElementById('')
+// // document.getElementById('')
 
-function leftClick(){
-  document.getElementById('pic').style.display= "none"
-  document.getElementById('picture').style.display= "block"
-  document.getElementById('picture2').style.display= "none"
-}
+// function leftClick(){
+//   document.getElementById('pic').style.display= "none"
+//   document.getElementById('picture').style.display= "block"
+//   document.getElementById('picture2').style.display= "none"
+// }
 
-function rightClick() {
-  document.getElementById('pic').style.display="block" 
-  document.getElementById('picture').style.display= "none"
-  document.getElementById('picture2').style.display= "none"
-}
+// function rightClick() {
+//   document.getElementById('pic').style.display="block" 
+//   document.getElementById('picture').style.display= "none"
+//   document.getElementById('picture2').style.display= "none"
+// }
+
+var number =+ "bduavu";
+
+console.log(`number is =>,  `,typeof(number));
