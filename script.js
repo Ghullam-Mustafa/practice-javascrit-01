f234ac96829b476209787f29f91ac3df45094b02
Ghullam Mustafa <gmustafawaince180@gmail.com>
2023-04-13 23:05:49 +0500

    write a programme celsius to fahernheit

--- script.js
+++ script.js
@@ -1,17 +1,21 @@
-// // document.getElementById('')
+// document.getElementById('')
 
-// function leftClick(){
-//   document.getElementById('pic').style.display= "none"
-//   document.getElementById('picture').style.display= "block"
-//   document.getElementById('picture2').style.display= "none"
-// }
+function leftClick(){
+  document.getElementById('pic').style.display= "none"
+  document.getElementById('picture').style.display= "block"
+  document.getElementById('picture2').style.display= "none"
+}
 
-// function rightClick() {
-//   document.getElementById('pic').style.display="block" 
-//   document.getElementById('picture').style.display= "none"
-//   document.getElementById('picture2').style.display= "none"
-// }
+function rightClick() {
+  document.getElementById('pic').style.display="block" 
+  document.getElementById('picture').style.display= "none"
+  document.getElementById('picture2').style.display= "none"
+}
 
-var number =+ "bduavu";
-
-console.log(`number is =>,  `,typeof(number));
+function temperature() {
+  var tempCelsius =+ prompt("enter celsious temperature");
+  var tempFahrenheit = ( tempCelsius * 9/5) + 32;
+  console.log(`Temperator in celsious is this ${tempCelsius} `);
+  console.log(`Temperator in fahrenheit is this ${tempFahrenheit} `);
+}
+temperature()
