24fbfee06b8cb265b3e01930cd65cd29fa68b593
Ghullam Mustafa <gmustafawaince180@gmail.com>
2023-04-14 23:31:26 +0500

    fcd fcf

--- script.js
+++ script.js
@@ -1,29 +1,36 @@
-// document.getElementById('')
+// // document.getElementById('')
 
-function leftClick(){
-  document.getElementById('pic').style.display= "none"
-  document.getElementById('picture').style.display= "block"
-  document.getElementById('picture2').style.display= "none"
-}
+// function leftClick(){
+//   document.getElementById('pic').style.display= "none"
+//   document.getElementById('picture').style.display= "block"
+//   document.getElementById('picture2').style.display= "none"
+// }
 
-function rightClick() {
-  document.getElementById('pic').style.display="block" 
-  document.getElementById('picture').style.display= "none"
-  document.getElementById('picture2').style.display= "none"
-}
+// function rightClick() {
+//   document.getElementById('pic').style.display="block" 
+//   document.getElementById('picture').style.display= "none"
+//   document.getElementById('picture2').style.display= "none"
+// }
 
-var anyVari =+ "fajfhdf"
-console.log(typeof(anyVari)); 
+// var anyVari =+ "fajfhdf"
+// console.log(typeof(anyVari)); 
 
-function temperature() {
-  var tempCelsius =+ prompt("enter celsious temperature");
-  var tempFahrenheit = ( tempCelsius * 9/5) + 32;
-  console.log(`Temperator in celsious is this ${tempCelsius} `);
-  console.log(`Temperator in fahrenheit is this ${tempFahrenheit} `);
-}
-temperature()
+// function temperature() {
+//   var tempCelsius =+ prompt("enter celsious temperature");
+//   var tempFahrenheit = ( tempCelsius * 9/5) + 32;
+//   console.log(`Temperator in celsious is this ${tempCelsius} `);
+//   console.log(`Temperator in fahrenheit is this ${tempFahrenheit} `);
+// }
+// temperature()
 
-var num1 = 9
-var num2 = 88
+// var num1 = 9
+// var num2 = 88
 
-console.log(`${num1} is less than is ${num2}`, num1 < num2);
+// console.log(`${num1} is less than is ${num2}`, num1 < num2);
+
+var num1 = 55
+var num2 = 45
+var num3 = 35
+var num4 = 25
+var total = num1 <= num2 || num3 >= num4
+console.log("total" ,total);
