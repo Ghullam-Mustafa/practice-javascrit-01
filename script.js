81dcc8aa95136d6d63958bbcff0db53e8eb0c7f8
Ghullam Mustafa <gmustafawaince180@gmail.com>
2023-04-17 08:49:17 +0500

    Even

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
-var numbOne = 5
-var numbTwo = 7
-if (numbOne == numbTwo) {
-  console.log("both values are equL");
+// var numbOne = 5
+// var numbTwo = 7
+// if (numbOne == numbTwo) {
+//   console.log("both values are equL");
+// }
+// else{
+//   console.log("both values are not equal");
+// }
+
+var numberOne = 21
+if (numberOne %2 == 0) {
+  console.log("weather number is even");
 }
 else{
-  console.log("both values are not equal");
+  console.log("Number is not even");
 }
