f807dae81c637291ce9f674ac369dbc5be3ab4d2
Ghullam Mustafa <gmustafawaince180@gmail.com>
2023-04-17 09:48:45 +0500

    switch

--- script.js
+++ script.js
@@ -15,22 +15,46 @@
 //   console.log("Number is not even");
 // }
 
-var score = 91
-if (score < 50) {
-  console.log("you are fail");
-}
-else if (score < 60) {
-  console.log("you are grade C");
-}
-else if (score < 70) {
-  console.log("you are grade B");
-}
-else if (score < 80) {
-  console.log("you are grade A");
-}
-else if (score < 90) {
-  console.log("you are grade A+");
-}
-else{
-  console.log("your grade is A+Gold");
+// var score = 72
+// if (score <= 50) {
+//   console.log("you are fail");
+// }
+// else if (score >= 60 && score <= 70) {
+//   console.log("you are grade C");
+// }
+// else if (score >= 70 && score <= 80) {
+//   console.log("you are grade B");
+// }
+// else if (score >= 80 && score <= 90) {
+//   console.log("you are grade A");
+// }
+// else  {
+//   console.log("you are grade A+");
+// }
+
+var day = "Sun"
+switch (day) {
+  case "Mon":
+    console.log("Today is Monday");
+    break;
+  case "Tus":
+    console.log("Today is Tuesday");
+    break;
+  case "Wed":
+    console.log("Today is Wednesday");
+    break;
+  case "Thur":
+    console.log("Today is Thursday");
+    break;
+  case "Fri":
+    console.log("Today is Friday");
+    break;
+  case "Sat":
+    console.log("Today is Saturday");
+    break;
+  case "Sun":
+    console.log("Today is Sunday");
+    break;
+  default:
+    break;
 }
