bd67a3911601ef720a7c4b1ce0abdaeb5b4df3f0
Ghullam Mustafa <gmustafawaince180@gmail.com>
2023-04-17 17:36:54 +0500

    for loop

--- script.js
+++ script.js
@@ -32,29 +32,54 @@
 //   console.log("you are grade A+");
 // }
 
-var day = "Sun"
-switch (day) {
-  case "Mon":
-    console.log("Today is Monday");
-    break;
-  case "Tus":
-    console.log("Today is Tuesday");
-    break;
-  case "Wed":
-    console.log("Today is Wednesday");
-    break;
-  case "Thur":
-    console.log("Today is Thursday");
-    break;
-  case "Fri":
-    console.log("Today is Friday");
-    break;
-  case "Sat":
-    console.log("Today is Saturday");
-    break;
-  case "Sun":
-    console.log("Today is Sunday");
-    break;
-  default:
-    break;
+// var day = "Sun"
+// switch (day) {
+//   case "Mon":
+//     console.log("Today is Monday");
+//     break;
+//   case "Tus":
+//     console.log("Today is Tuesday");
+//     break;
+//   case "Wed":
+//     console.log("Today is Wednesday");
+//     break;
+//   case "Thur":
+//     console.log("Today is Thursday");
+//     break;
+//   case "Fri":
+//     console.log("Today is Friday");
+//     break;
+//   case "Sat":
+//     console.log("Today is Saturday");
+//     break;
+//   case "Sun":
+//     console.log("Today is Sunday");
+//     break;
+//   default:
+//     break;
+// }
+
+
+// var num1 = 88
+// var num2 = 65
+// var num3 = 95
+// if (num1 > num2  || num2 > num3 || num3 > num2 ) {
+//   console.log(`nuber one is this${num1} and second number is this ${num2 } and maximum number is this ${num3}`);
+// }
+// else{
+//   console.log(`nnuber one is this${num1} and second number is this ${num2 } and maximum number is this ${num2}`);
+// }
+
+// var num1 = 5
+// var num2 = 11
+// if (num1 %5 == 0 || num2 %11 == 0) {
+//   console.log("5 and 11 are divisible");
+// }
+// else{
+//   console.log("5 and 11 are not dividesible");
+// }
+
+for (let index = 0; index < 10 ; index++) {
+
+  console.log("fassd");
 }
