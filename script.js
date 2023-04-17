4c84e76057e7ee95e1f815ca4910718f02f0f7f6
Ghullam Mustafa <gmustafawaince180@gmail.com>
2023-04-17 09:08:11 +0500

    grades

--- script.js
+++ script.js
@@ -7,10 +7,30 @@
 //   console.log("both values are not equal");
 // }
 
-var numberOne = 21
-if (numberOne %2 == 0) {
-  console.log("weather number is even");
+// var numberOne = 21
+// if (numberOne %2 == 0) {
+//   console.log("weather number is even");
+// }
+// else{
+//   console.log("Number is not even");
+// }
+
+var score = 91
+if (score < 50) {
+  console.log("you are fail");
+}
+else if (score < 60) {
+  console.log("you are grade C");
+}
+else if (score < 70) {
+  console.log("you are grade B");
+}
+else if (score < 80) {
+  console.log("you are grade A");
+}
+else if (score < 90) {
+  console.log("you are grade A+");
 }
 else{
-  console.log("Number is not even");
+  console.log("your grade is A+Gold");
 }
